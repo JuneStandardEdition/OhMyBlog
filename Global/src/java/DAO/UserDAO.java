33f4ca4012793026e067f4a48f53402863855b84
QL <llopes.kentin@gmail.com>
2020-04-24 15:49:27 +0200

    0.2.4: userDAO

--- Global/src/java/DAO/UserDAO.java
+++ Global/src/java/DAO/UserDAO.java
@@ -1,44 +1,102 @@
 package DAO;
 
 import beans.User;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author June
  */
 public class UserDAO implements DAO<User> {
 
+    public List<User> udao = new ArrayList<>();
+
     @Override
     public User find(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        User usr = null;
+        try {
+            String req = "SELECT * FROM user WHERE id = ?";
+            PreparedStatement pstmt = CONNEXION.prepareStatement(req);
+            pstmt.setInt(1, id);
+            ResultSet result = pstmt.executeQuery();
+            if (result.first()) {
+                usr = new User(result.getString("name"), result.getString("email"), result.getString("password"), result.getBoolean("isAdmin"));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return usr;
     }
 
     @Override
-    public void create(User T) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void create(User u) {
+        try {
+            String req = "INSERT INTO user (name, email, password) VALUES (?,?,?)";
+            PreparedStatement pstmt = CONNEXION.prepareStatement(req);
+            pstmt.setString(1, u.getName());
+            pstmt.setString(2, u.getEmail());
+            pstmt.setString(3, u.getPassword());
+            pstmt.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
     public void delete(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            String req = "DELETE FROM user WHERE id = ?";
+            PreparedStatement pstmt = CONNEXION.prepareStatement(req);
+            pstmt.setInt(1, id);
+            pstmt.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
-    public void update(User T) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void update(User u) {
+        try {
+            String req = "UPDATE user SET name = ?, email = ?, password = ?, isAdmin = ? WHERE id = ?";
+            PreparedStatement pstmt = CONNEXION.prepareStatement(req);
+            pstmt.setString(1, u.getName());
+            pstmt.setString(2, u.getEmail());
+            pstmt.setString(3, u.getPassword());
+            pstmt.setBoolean(4, u.isIsAdmin());
+            pstmt.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
     public List<User> getAll() {
+        String sqlQuery = "SELECT * FROM user";
+        try (Statement stmt = CONNEXION.createStatement()) {
+            ResultSet resSetUser = stmt.executeQuery(sqlQuery);
+            while (resSetUser.next()) {
+                udao.add(new User(resSetUser.getString("name"), resSetUser.getString("email"), resSetUser.getString("password"), resSetUser.getBoolean("isAdmin")));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return udao;
+        /*
         List<User> users = new ArrayList<>();
         users.add(new User("toto", "toto", "toto", true));
         users.add(new User("tata", "tata", "tata", true));
         return users;
+         */
     }
     
+    // Fonction temporaire
     public User findByMail(String email) {
-        
         return null;
     }
     
